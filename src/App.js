2e8b2f94554bed55289c877a9c7cc2a60d3ed00c
shakti97 <shaktisingh230797@gmail.com>
2018-11-13 11:48:55 +0530

    Refactor App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Switch ,Route } from 'react-router';
+import {Link} from 'react-router-dom';
+import BeforeHOCComponenet1 from "./Components/BeforeHOCComponent1";
+import BeforeHOCComponenet2 from "./Components/BeforeHOCComponent2";
 
+
+const li= styled.li`
+    display : "inline"
+`;
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <body>
+          <ul>
+            <li ><Link to="/Component1">Component1</Link></li>
+            <li><Link to="/Component2">Component2</Link></li>
+          </ul>
+          <Switch>
+            <Route exact path="/" component={BeforeHOCComponenet1}/>
+            <Route path="/Component1" component={BeforeHOCComponenet1}/>
+            <Route path="/Component2" component={BeforeHOCComponenet2}/>
+
+          </Switch>
+        </body>
       </div>
     );
   }
